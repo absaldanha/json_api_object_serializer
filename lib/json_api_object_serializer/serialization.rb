58b1449a2c1a7f043a21c3061bc79563aa93018c
Alexandre Saldanha <alexandre.bsaldanha@gmail.com>
2018-09-18 19:36:43 -0300

    Add support for resource collection serialization
    - Adds new option for serialization methods to identify if it is a collection or not (`collection: true`)
    - Adds verifications to return the correct empty serialized hash for `nil` values and empty collections

--- lib/json_api_object_serializer/serialization.rb
+++ lib/json_api_object_serializer/serialization.rb
@@ -6,19 +6,35 @@ module JsonApiObjectSerializer
       base.extend DSL
     end
 
-    def to_hash(resource)
-      resource_hash(resource)
+    def to_hash(resource, **options)
+      if options[:collection]
+        serialized_collection(resource, **options)
+      else
+        serialized_hash(resource, **options)
+      end
     end
 
     private
 
-    def resource_hash(resource)
+    def serialized_hash(resource, **_options)
       result_hash = { data: nil }
+
+      return result_hash if resource.nil?
+
       result_hash[:data] = identifier_of(resource)
       result_hash[:data].merge!(attributes_from(resource))
       result_hash[:data].merge!(relationships_from(resource))
 
       result_hash
+    end
+
+    def serialized_collection(resource_collection, **_options)
+      return { data: [] } if resource_collection.nil?
+
+      resource_collection.each_with_object(data: []) do |resource, result_hash|
+        serialized_resource_hash = serialized_hash(resource)[:data]
+        result_hash[:data].push(serialized_resource_hash) unless serialized_resource_hash.nil?
+      end
     end
 
     def identifier_of(resource)
